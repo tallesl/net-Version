41bb1d0de5a59dcb295e44c64b9c46ccf7dbf349
Talles L <talleslasmar@gmail.com>
2015-03-12 15:39:34 -0300

    existing code

---
+++ ApplicationVersionHtmlHelper/ApplicationVersion.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace ApplicationVersionHtmlHelper
+{
+    /// <summary>
+    /// ASP.NET MVC HTML Helper that gives current assembly version.
+    /// </summary>
+    public static class ApplicationVersionHtmlHelper
+    {
+        /// <summary>
+        /// Returns the version of the current assembly.
+        /// </summary>
+        /// <param name="html">Html helper with the context to get the version from</param>
+        /// <returns>Assembly's version</returns>
+        public static string ApplicationVersion(this HtmlHelper html)
+        {
+            return html.ViewContext.HttpContext
+                .ApplicationInstance.GetType().BaseType
+                .Assembly.GetName().Version.ToString();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# ApplicationVersionHtmlHelper
+
+[![nuget package](https://badge.fury.io/nu/ApplicationVersionHtmlHelper.png)](http://badge.fury.io/nu/ApplicationVersionHtmlHelper)
+
+
+I have used [this little snippet](http://stackoverflow.com/a/9486407) countless times to show the system version number on it's footer.
+
+*NuGet package* all the things.
+
+![Nuget package all the things](all-the-things.png)
